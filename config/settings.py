c5c688377d1b6fe781e8bc6baa81aaf4a6ed64d8
yakubjanov004 <yoqubjonov229@gmail.com>
2024-12-21 17:00:51 +0500

    TokenAuthentication

--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,14 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+}
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
