f6d26080f58069fc7b6b3b51496dbf10dd324108
Bakirali_Zokirov <160619651+bakirali1212@users.noreply.github.com>
2025-01-04 14:03:34 +0500

    14:03

--- config/settings.py
+++ config/settings.py
@@ -116,3 +116,11 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'userapp.UserModel'
+
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
