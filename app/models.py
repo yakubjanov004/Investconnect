bc5413f22536932359dacb09e33d448124c68d9c
Bakirali_Zokirov <160619651+bakirali1212@users.noreply.github.com>
2025-01-11 15:59:20 +0500

    salom

--- app/models.py
+++ app/models.py
@@ -21,7 +21,7 @@ class Product(BaseModel):
     category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     rendement = models.CharField(max_length=5)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-
+    is_active = models.BooleanField(default=False)
     def __str__(self):
         return self.name
 
@@ -38,7 +38,17 @@ class PrivateInformation(BaseModel):
     oylik_daromadi = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     soff_foydasi = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     def __str__(self):
-        return f"{self.product.name} | {self.key}: {self.value}"
+        return f"{self.product.name} "
+
+class Payment(models.Model):
+    investor = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=False)  # To'lovdan keyin faollashadi
+
+    def __str__(self):
+        return f"Payment for {self.product.name} by {self.investor.username} - ${self.amount}"
 
 
 class Comment(BaseModel):
