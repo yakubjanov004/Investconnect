3461be70ea8f99fc69f55c1fbf2a80d756096cf4
yakubjanov004 <yoqubjonov229@gmail.com>
2025-01-11 07:03:35 +0000

    server push

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from userapp.models import UserModel  
-from userapp.base import BaseModel  
+from userapp.models import UserModel
+from userapp.base import BaseModel
 
 
 
@@ -17,8 +17,8 @@ class Product(BaseModel):
     description = models.TextField()
     location = models.CharField(max_length=100)
     image = models.ImageField(upload_to="products/")
-    user = models.ForeignKey(UserModel, on_delete=models.PROTECT)
-    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    user = models.ForeignKey(UserModel, on_delete=models.PROTECT, null=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     rendement = models.CharField(max_length=5)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
@@ -30,22 +30,21 @@ class PrivateInformation(BaseModel):
     class DegreeChoicess(models.TextChoices):
         AKTIV = 'aktiv', 'Aktiv'
         DEAKTIV = 'deaktiv', 'Deaktiv'
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, null=True)
     status = models.CharField(max_length=30, choices=DegreeChoicess.choices, default=DegreeChoicess.AKTIV)
     kampanya_egasi = models.CharField(max_length=50)
     kontact = models.CharField(max_length=50)
     campany_name = models.CharField(max_length=50)
-    oylik_daromadi = models.DecimalField(max_digits=10, decimal_places=2, default=0.00) 
+    oylik_daromadi = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     soff_foydasi = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     def __str__(self):
         return f"{self.product.name} | {self.key}: {self.value}"
 
 
 class Comment(BaseModel):
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
-    user = models.ForeignKey(UserModel, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT, null=True)
+    user = models.ForeignKey(UserModel, on_delete=models.PROTECT, null=True)
     description = models.TextField()
 
     def __str__(self):
         return f"{self.product.name} | {self.user.username}: {self.description[:30]}..."
-    
